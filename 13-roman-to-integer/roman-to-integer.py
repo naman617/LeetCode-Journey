926a751667062b56a3bff9b0fb55164abddfce66
Naman Nandana <53851970+naman617@users.noreply.github.com>
2025-06-25 14:51:16 -0700

    Time: 8 ms (27.21%) | Memory: 17.9 MB (39.81%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -3,11 +3,10 @@ class Solution:
         map={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         res=0
         for i in range(len(s)):
-            if (i+1) < len(s) and map[s[i]] < map[s[i+1]]:
+            if i+1<len(s) and map[s[i]]<map[s[i+1]]:
                 res-=map[s[i]]
             else:
                 res+=map[s[i]]
         return res
-
 
         
