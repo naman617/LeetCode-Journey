448db0ce6963dd405dc774ea91d23023a8f0613b
Naman Nandana <53851970+naman617@users.noreply.github.com>
2025-07-21 23:54:12 -0700

    Time: 86 ms (56.53%) | Memory: 21.3 MB (56.21%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -11,32 +11,28 @@ class Codec:
         res=[]
         def dfs(node):
             if not node:
-                res.append("N")
+                res.append('N')
                 return
             res.append(str(node.val))
             dfs(node.left)
             dfs(node.right)
         dfs(root)
-        return ",".join(res)
-            
-
-        
+        return ','.join(res)
         
 
     def deserialize(self, data):
-        vals=data.split(",")
+        vals=data.split(',')
         self.i=0
         def dfs():
-            if vals[self.i]=="N":
+            if vals[self.i]=='N':
                 self.i+=1
                 return None
-            node=TreeNode(int(vals[self.i]))
+            node=TreeNode(vals[self.i])
             self.i+=1
             node.left=dfs()
             node.right=dfs()
             return node
         return dfs()
-        
         
 
 # Your Codec object will be instantiated and called as such:
