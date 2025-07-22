1e377ee08581cb9df9839b9d14644948ca066d16
Naman Nandana <53851970+naman617@users.noreply.github.com>
2025-07-22 15:20:15 -0700

    Time: 59 ms (88.85%) | Memory: 18.2 MB (77.48%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,48 +1,26 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
+        if t=="":
             return ""
+        window,countT={},{}
+        for c in t:
+            countT[c]=1+countT.get(c,0)
+        res,resLen=[-1,-1],float('inf')
+        l=0
+        have,need=0,len(countT)
+        for r in range(len(s)):
+            c=s[r]
+            window[c]=1+window.get(c,0)
+            if c in countT and window[c]==countT[c]:
+                have+=1
+            while have==need:
+                if (r-l+1)<resLen:
+                    res=[l,r]
+                    resLen=(r-l+1)
+                window[s[l]]-=1
+                if s[l] in countT and window[s[l]]<countT[s[l]]:
+                    have-=1
+                l+=1
+        l,r=res
+        return s[l:r+1] if resLen!=float('inf') else ""
 
-    
-        t_counts = collections.Counter(t)
-
-    
-        left = 0
-        required = len(t_counts)
-        formed = 0
-
-        window_counts = {}
-
-    
-        ans = (float('inf'), None, None)
-
-    
-        for right, char in enumerate(s):
-        
-            window_counts[char] = window_counts.get(char, 0) + 1
-
-        
-            if char in t_counts and window_counts[char] == t_counts[char]:
-                formed += 1
-        
-        
-            while left <= right and formed == required:
-            
-                if (right - left + 1) < ans[0]:
-                    ans = (right - left + 1, left, right)
-
-            
-                left_char = s[left]
-
-            
-                window_counts[left_char] -= 1
-
-            
-                if left_char in t_counts and window_counts[left_char] < t_counts[left_char]:
-                    formed -= 1
-
-           
-                left += 1
-
-   
-        return "" if ans[0] == float('inf') else s[ans[1] : ans[2] + 1]
