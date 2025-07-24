9c2409cd7fc62d0e971a698e59637657c9898b0f
Naman Nandana <53851970+naman617@users.noreply.github.com>
2025-07-24 14:36:16 -0700

    Time: 23 ms (18.34%) | Memory: 22.8 MB (12.89%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -6,9 +6,5 @@ class Solution:
             for c in s:
                 count[ord(c)-ord('a')]+=1
             res[tuple(count)].append(s)
-        return(list(res.values()))
-
-
-
-        
+        return list(res.values())
         
